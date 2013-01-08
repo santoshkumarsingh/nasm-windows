deffee7eefd490fb46dabf3388779d73e57d3fd8
unknown <santosh@santosh-PC.(none)>
2013-01-08 21:18:58 +0530

    Add two number in nasm

--- add2number.asm
+++ add2number.asm
@@ -1,5 +1,32 @@
 section .data
+    
+    msg : db "Sum=%d",10,0
 
 
 section .text
+    extern _printf
+    global _main
 
+_main:
+    ;create stack
+    push ebp
+    mov esp,ebp
+    ;store first value in eax register
+    mov eax,45
+    ;add second value in eax and store the value in eax
+    add eax,45
+    
+    push eax
+    
+    push msg
+    
+    call _printf
+    
+    pop ebp
+    
+    mov esp,ebp
+    
+    ret
+    
+    
+    
